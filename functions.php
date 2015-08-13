110d30f9ad1ceefce3f6da2bce4221d07939586d
Melissa Cabral <melissacabral@gmail.com>
2015-08-12 18:35:09 -0700

    starting the theme

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+//activate sleeping wordpress features
+add_theme_support( 'post-thumbnails' );
+
+//add any custom image sizes you need for banners, ads, etc
+//					name 	 width  height  crop?
+add_image_size( 'big-banner', 1065, 250, true );
+
+
+//no close PHP
